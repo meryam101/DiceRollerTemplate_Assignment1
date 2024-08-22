cbeb721cd3f18b9d5234e42d4da0dd62c533af9a
Meryam101 <mbr339.my@gmail.com>
2024-08-22 12:45:14 +1000

    initial commit

--- dice.py
+++ dice.py
@@ -5,3 +5,4 @@ def roll_dice(type_d):
     dice_range = range(1, type_d + 1)
     face_value = random.choice(dice_range)
     return face_value
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyscript import document  # webpage module links
+from pyscript import document # webpage module links
 # your roll_dice function should be saved in a file named 'dice.py'
 # uncomment the next line when you have this prepared
 import dice
@@ -6,19 +6,29 @@ import dice
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = "Coin"
-
+dice_type = "2"
+number_of_dice = 0
 
 def select_face_option(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-
+    dice_type = document.querySelector("#dice_type").value
 
 def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
 
-    print(dice.roll_dice(2))
+    rolls = int(document.querySelector("#rolls").value)
+    while rolls > 0:
+        result = dice.roll_dice(int(dice_type))
+        res_string = str(result)
+        document.querySelector("div#roll-history").innerHTML += "<p>" + res_string + "</p>"
+        rolls -= 1
+
+
+
+
+
 
 
 def clear_history(event):
